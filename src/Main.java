f47e4a6566a82ee972c9c343f8118e69206fafb2
Kilimnik <Apollo.Strict@gmail.com>
2023-06-22 14:13:07 +0300

    Add object

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int cost = 18438; // стоимость билета
-        int mile = 20; // количество рублей для начисления одной бонусной мили
-        int bonus = cost / mile;
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
 
-        System.out.println("Начислено бонусных миль за покупку билета:");
-        System.out.println((bonus) + " миль");
     }
 }
